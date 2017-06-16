d35c9db022a95ade5cbac3eceacd52702d5eeeb9
Andras Timar <andras.timar@collabora.com>
2017-06-16 12:27:31 +0200

    Install hyphenation patterns, fixes #13

--- scripts/install-libreoffice.sh
+++ scripts/install-libreoffice.sh
@@ -9,6 +9,9 @@ apt-get -y install apt-transport-https
 # Install locales
 apt-get -y install locales-all
 
+# Install hyphenation patterns
+apt-get -y install hyphen-*
+
 # Add Collabora repos
 echo "deb https://collaboraoffice.com/repos/CollaboraOnline/CODE /" >> /etc/apt/sources.list.d/collabora.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6CCEA47B2281732DF5D504D00C54D189F4BA284D
