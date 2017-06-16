00ded50474d1222d9f0868a941d0a7a022bae752
Andras Timar <andras.timar@collabora.com>
2017-06-16 12:58:34 +0200

    Parametrize CN for certificate for custom domain, fixes #1

--- scripts/start-libreoffice.sh
+++ scripts/start-libreoffice.sh
@@ -14,7 +14,11 @@ openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/c
 mkdir -p certs/{servers,tmp}
 mkdir -p "certs/servers/localhost"
 openssl genrsa -out "certs/servers/localhost/privkey.pem" 2048 -key "certs/servers/localhost/privkey.pem"
+if test "${cert_domain-set}" == set; then
 openssl req -key "certs/servers/localhost/privkey.pem" -new -sha256 -out "certs/tmp/localhost.csr.pem" -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=localhost"
+else
+openssl req -key "certs/servers/localhost/privkey.pem" -new -sha256 -out "certs/tmp/localhost.csr.pem" -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=${cert_domain}"
+fi
 openssl x509 -req -in certs/tmp/localhost.csr.pem -CA certs/ca/root.crt.pem -CAkey certs/ca/root.key.pem -CAcreateserial -out certs/servers/localhost/cert.pem -days 9131
 mv certs/servers/localhost/privkey.pem /etc/loolwsd/key.pem
 mv certs/servers/localhost/cert.pem /etc/loolwsd/cert.pem
