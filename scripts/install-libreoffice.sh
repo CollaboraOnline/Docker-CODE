6c5588a344970a94a11f1ef08a383423f7e07c43
Andras Timar <andras.timar@collabora.com>
2017-12-17 20:59:51 +0100

    install langpacks

--- scripts/install-libreoffice.sh
+++ scripts/install-libreoffice.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# Update installed packages
+# Update installed packages
 apt-get update && apt-get -y upgrade
 
-# Install HTTPS transport
+# Install HTTPS transport
 apt-get -y install apt-transport-https
 
 # Install locales
 apt-get -y install locales-all
 
-# Add Collabora repos
+# Add Collabora repos
 echo "deb https://collaboraoffice.com/repos/CollaboraOnline/CODE /" >> /etc/apt/sources.list.d/collabora.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6CCEA47B2281732DF5D504D00C54D189F4BA284D
 apt-get update
 
 # Install the Collabora packages
-apt-get -y install loolwsd code-brand collaboraoffice5.3-dict*
+apt-get -y install loolwsd code-brand collaboraoffice5.3-dict* collaboraofficebasis5.3*
 
 # Copy dictionaries to paths where LOK recognizes them
 mkdir -p /usr/share/hunspell
