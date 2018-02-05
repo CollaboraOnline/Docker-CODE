f0e82b4eb574cf8bfca1708ac64c4da1803fee22
Andras Timar <andras.timar@collabora.com>
2018-02-05 20:32:47 +0000

    copy only those spelling dictionaries that are enabled by default in allowed_languages setting

--- scripts/install-libreoffice.sh
+++ scripts/install-libreoffice.sh
@@ -26,8 +26,6 @@ for i in `find /opt/collaboraoffice5.3/share/extensions/ -name hyph*.dic`;do cp 
 cp /opt/collaboraoffice5.3/share/extensions/dict-en/en_US.* /usr/share/hunspell
 cp /opt/collaboraoffice5.3/share/extensions/dict-en/en_GB.* /usr/share/hunspell
 cp /opt/collaboraoffice5.3/share/extensions/dict-it/it_IT.* /usr/share/hunspell
-cp /opt/collaboraoffice5.3/share/extensions/dict-nl/nl_NL.* /usr/share/hunspell
-cp /opt/collaboraoffice5.3/share/extensions/dict-pl/pl_PL.* /usr/share/hunspell
 cp /opt/collaboraoffice5.3/share/extensions/dict-ru/ru_RU.* /usr/share/hunspell
 cp /opt/collaboraoffice5.3/share/extensions/dict-pt-BR/pt_BR.* /usr/share/hunspell
 cp /opt/collaboraoffice5.3/share/extensions/dict-pt-PT/pt_PT.* /usr/share/hunspell
@@ -35,10 +33,6 @@ cp /opt/collaboraoffice5.3/share/extensions/dict-fr/fr.dic /usr/share/hunspell/f
 cp /opt/collaboraoffice5.3/share/extensions/dict-fr/fr.aff /usr/share/hunspell/fr_FR.aff
 cp /opt/collaboraoffice5.3/share/extensions/dict-es/es_ANY.dic /usr/share/hunspell/es_ES.dic
 cp /opt/collaboraoffice5.3/share/extensions/dict-es/es_ANY.aff /usr/share/hunspell/es_ES.aff
-cp /opt/collaboraoffice5.3/share/extensions/dict-de/de_AT_frami.dic /usr/share/hunspell/de_AT.dic
-cp /opt/collaboraoffice5.3/share/extensions/dict-de/de_AT_frami.aff /usr/share/hunspell/de_AT.aff
-cp /opt/collaboraoffice5.3/share/extensions/dict-de/de_CH_frami.dic /usr/share/hunspell/de_CH.dic
-cp /opt/collaboraoffice5.3/share/extensions/dict-de/de_CH_frami.aff /usr/share/hunspell/de_CH.aff
 cp /opt/collaboraoffice5.3/share/extensions/dict-de/de_DE_frami.dic /usr/share/hunspell/de_DE.dic
 cp /opt/collaboraoffice5.3/share/extensions/dict-de/de_DE_frami.aff /usr/share/hunspell/de_DE.aff
 apt-get -y remove --purge collaboraoffice5.3-dict*
