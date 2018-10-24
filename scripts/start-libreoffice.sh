ecf7c29d4596243e87635992da3c4aa0c2f2f65b
Andras Timar <andras.timar@collabora.com>
2018-10-24 14:26:15 +0200

    pass extra loolwsd command line parameter via environment variable
    docker run .... -e "extra_params=....." .....
    
    For example it makes sense to start loolwsd without SSL, when you test
    or develop. in this case the syntax is: -e "extra_params=--o:ssl.enable=false"

--- scripts/start-libreoffice.sh
+++ scripts/start-libreoffice.sh
@@ -33,4 +33,4 @@ perl -pi -e "s/<server_name (.*)>.*<\/server_name>/<server_name \1>${server_name
 perl -pi -e "s/<allowed_languages (.*)>.*<\/allowed_languages>/<allowed_languages \1>${dictionaries:-de_DE en_GB en_US es_ES fr_FR it nl pt_BR pt_PT ru}<\/allowed_languages>/" /etc/loolwsd/loolwsd.xml
 
 # Start loolwsd
-su -c "/usr/bin/loolwsd --version --o:sys_template_path=/opt/lool/systemplate --o:lo_template_path=/opt/collaboraoffice5.3 --o:child_root_path=/opt/lool/child-roots --o:file_server_root_path=/usr/share/loolwsd" -s /bin/bash lool
+su -c "/usr/bin/loolwsd --version --o:sys_template_path=/opt/lool/systemplate --o:lo_template_path=/opt/collaboraoffice5.3 --o:child_root_path=/opt/lool/child-roots --o:file_server_root_path=/usr/share/loolwsd ${extra_params}" -s /bin/bash lool
