4c9839b4e45da69cc67a01c4099749491543092d
Andras Timar <andras.timar@collabora.com>
2020-03-05 13:10:13 +0100

    don't start another shell, let the loolwsd process have the PID 1

--- scripts/start-libreoffice.sh
+++ scripts/start-libreoffice.sh
@@ -41,4 +41,4 @@ perl -pi -e "s/<allowed_languages (.*)>.*<\/allowed_languages>/<allowed_language
 
 
 # Start loolwsd
-/bin/bash -c "/usr/bin/loolwsd --version --o:sys_template_path=/opt/lool/systemplate --o:child_root_path=/opt/lool/child-roots --o:file_server_root_path=/usr/share/loolwsd ${extra_params}"
+exec /usr/bin/loolwsd --version --o:sys_template_path=/opt/lool/systemplate --o:child_root_path=/opt/lool/child-roots --o:file_server_root_path=/usr/share/loolwsd ${extra_params}
